e7289c30934d6da30cd65eca1795b4dbd1d001c5
Tatsiana Manyshava <119797324+manyshevat@users.noreply.github.com>
2022-12-04 17:20:07 +0300

    Update streamlit_app.py
    Add Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.title('Breakfast menu')
+streamlit.title('Kale, Spinach & Rocket Smuthie')
+streamlit.title('Hard-Boiled Free-Ranged Egg')
